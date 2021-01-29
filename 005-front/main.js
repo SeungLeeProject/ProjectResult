8b9a2e2f8e99e737d0e44db7a9e82e9a55b67c05
DESKTOP-7DOOLT9\ckstl <ckstlr0828@naver.com>
2021-01-29 15:26:10 +0900

    main화면 1차

---
+++ 005-front/main.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('#section_1').slick({
+        dots : true,
+        autoplay : true,
+        autoplaySpeed : 3000,
+        slidesToShow: 5,
+        slidesToScroll: 1
+    });
+});
