c715688acc6e4ab407f932d68e4d5fa7e69d486b
DESKTOP-7DOOLT9\ckstl <ckstlr0828@naver.com>
2021-01-30 14:01:13 +0900

    메인 페이지 다시 재구성

--- 005-front/main.js
+++ 005-front/main.js
@@ -1,9 +1,11 @@
-$(function(){
-    $('#section_1').slick({
-        dots : true,
-        autoplay : true,
-        autoplaySpeed : 3000,
-        slidesToShow: 5,
-        slidesToScroll: 1
-    });
-});
+$(function(){	
+    $('.bxslider').bxSlider({
+        mode:"horizontal",
+        slideWidth: 288, // 슬라이드의 가로사이즈,
+        minSlides : 5, //최소해상도에서 나오는 슬라이드 개수
+        maxSlides : 5, //최대해상도에서 나오는 슬라이드 개수
+        moveSlides : 1, //슬라이드 이동하는 개수
+        slideMargin : 40, //각 슬라이드의 여백
+        pager: true,
+      });
+  });
