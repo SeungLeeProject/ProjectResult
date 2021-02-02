609d543d2324010e268964956e138cfec233b9c2
DESKTOP-7DOOLT9\ckstl <ckstlr0828@naver.com>
2021-02-02 19:54:43 +0900

    섹션 1은 마무리

--- 005-front - 진짜 진찌/main.js
+++ 005-front - 진짜 진찌/main.js
@@ -1,11 +1,3 @@
 $(function(){	
-    $('.bxslider').bxSlider({
-        mode:"horizontal",
-        slideWidth: 288, // 슬라이드의 가로사이즈,
-        minSlides : 5, //최소해상도에서 나오는 슬라이드 개수
-        maxSlides : 5, //최대해상도에서 나오는 슬라이드 개수
-        moveSlides : 1, //슬라이드 이동하는 개수
-        slideMargin : 40, //각 슬라이드의 여백
-        pager: true,
-    });
+    
 });
