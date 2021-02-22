6e6cc8239f8dc7ff2807385a1a6092a2a26fe89d
DESKTOP-7DOOLT9\ckstl <ckstlr0828@naver.com>
2021-02-22 11:16:23 +0900

    로그인 페이지까지 완성 메인페이지 js, css, html  수정

--- SL_Webproj/src/main/webapp/resources/js/header.js
+++ SL_Webproj/src/main/webapp/resources/js/header.js
@@ -5,7 +5,7 @@ $(function(){
     $(".searchbtn").click(function(){
         $searchbt++;
         $search_on_off=$searchbt%2;
-        
+
         if($search_on_off==1){
             $(".search_wrap").css("top","3rem");
             $(this).css("background-image","url('image/icon/header-icon-3-3.png')");
@@ -13,6 +13,7 @@ $(function(){
             $('.writingbtn').css("background-image","url('image/icon/header-icon-4-2.png')");
             $('.mypage_dropbox').css("background-image","url('image/icon/header-icon-5-2.png')");
             $("#searchbg").fadeIn();
+            $("body").css("overflow","hidden");
             $(this).addClass("searchon");
         }else{
             $(".search_wrap").css("top","-30rem");
@@ -21,7 +22,45 @@ $(function(){
             $('.writingbtn').css("background-image","url('image/icon/header-icon-4-1.png')");
             $('.mypage_dropbox').css("background-image","url('image/icon/header-icon-5-1.png')");
             $("#searchbg").fadeOut();
+            $("body").css("overflow","auto");
             $(this).removeClass("searchon");
+        }
+    });
+    $(".searchclose").click(function(){
+        $(".search_wrap").css("top","-30rem");
+        $(".searchbtn").css("background-image","url('image/icon/header-icon-3-1.png')");
+        $('.language').css("background-image","url('image/icon/header-icon-2-1.png')");
+        $('.writingbtn').css("background-image","url('image/icon/header-icon-4-1.png')");
+        $('.mypage_dropbox').css("background-image","url('image/icon/header-icon-5-1.png')");
+        $("#searchbg").fadeOut();
+        $("body").css("overflow","auto");
+        $(".searchbtn").removeClass("searchon");
+    });
+
+    var $reactivemenubtn = 0;
+    var $reactivemenu_on_off = 0;
+    $(".reactive_menu_wrap").click(function(){
+        $reactivemenubtn++;
+        $reactivemenu_on_off=$reactivemenubtn%2;
+        
+        if($reactivemenu_on_off==1){
+            $("nav").fadeIn();
+            $(".reactive_menu_close_icon").fadeIn();
+            $(".reactive_menu_icon").fadeOut();
+            $('body').css("overflow","hidden");
+            $('.language').css("background-image","url('image/icon/header-icon-2-2.png')");
+            $('.searchbtn').css("background-image","url('image/icon/header-icon-3-2.png')");
+            $('.writingbtn').css("background-image","url('image/icon/header-icon-4-2.png')");
+            $('.mypage_dropbox').css("background-image","url('image/icon/header-icon-5-2.png')");
+        }else{
+            $("nav").fadeOut();
+            $(".reactive_menu_close_icon").fadeOut();
+            $(".reactive_menu_icon").fadeIn();
+            $('body').css("overflow","auto");
+            $('.language').css("background-image","url('image/icon/header-icon-2-1.png')");
+            $('.searchbtn').css("background-image","url('image/icon/header-icon-3-1.png')");
+            $('.writingbtn').css("background-image","url('image/icon/header-icon-4-1.png')");
+            $('.mypage_dropbox').css("background-image","url('image/icon/header-icon-5-1.png')");
         }
     });
 
