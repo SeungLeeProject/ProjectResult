492dfe0e122752e18ba10ea243a2d8ea0f764c51
Keun Young Song <gypig0927@gmail.com>
2021-03-04 15:08:54 +0900

    경로변경

--- SL_Webproj/src/main/webapp/resources/js/aside.js
+++ SL_Webproj/src/main/webapp/resources/js/aside.js
@@ -5,19 +5,19 @@ $(document).ready(function(){
         var $asideli = $("#aside_2 li");
 
         if($scrollstart <= $("#con_1").offset().top){
-            $asideli.eq(0).css("background-image","url('../../resources/images/icon/aside-icon-1-2.png')");
+            $asideli.eq(0).css("background-image","url('/resources/images/icon/aside-icon-1-2.png')");
         }else{
-            $asideli.eq(0).css("background-image","url('../../resources/images/icon/aside-icon-1-1.png')");
+            $asideli.eq(0).css("background-image","url('/resources/images/icon/aside-icon-1-1.png')");
         };
         if($scrollstart > $("#con_1").offset().top && $scrollstart <= $("#con_2").offset().top){
-            $asideli.eq(1).css("background-image","url('../../resources/images/icon/aside-icon-2-2.png')");
+            $asideli.eq(1).css("background-image","url('/resources/images/icon/aside-icon-2-2.png')");
         }else{
-            $asideli.eq(1).css("background-image","url('../../resources/images/icon/aside-icon-2-1.png')");
+            $asideli.eq(1).css("background-image","url('/resources/images/icon/aside-icon-2-1.png')");
         };
         if($scrollstart > $("#con_2").offset().top && $scrollstart <= $("#con_3").offset().top){
-            $asideli.eq(2).css("background-image","url('../../resources/images/icon/aside-icon-3-2.png')");
+            $asideli.eq(2).css("background-image","url('/resources/images/icon/aside-icon-3-2.png')");
         }else{
-            $asideli.eq(2).css("background-image","url('../../resources/images/icon/aside-icon-3-1.png')");
+            $asideli.eq(2).css("background-image","url('/resources/images/icon/aside-icon-3-1.png')");
         };
     });
 
@@ -25,40 +25,40 @@ $(document).ready(function(){
     var $topbt = $("#topbt");
 
     $asideli.eq(0).hover(function(){
-        $asideli.eq(0).css("background-image","url('../../resources/images/icon/aside-icon-1-2.png')");
+        $asideli.eq(0).css("background-image","url('/resources/images/icon/aside-icon-1-2.png')");
     }, function(){
-        $asideli.eq(0).css("background-image","url('../../resources/images/icon/aside-icon-1-1.png')");
+        $asideli.eq(0).css("background-image","url('/resources/images/icon/aside-icon-1-1.png')");
     });
     $asideli.eq(1).hover(function(){
-        $asideli.eq(1).css("background-image","url('../../resources/images/icon/aside-icon-2-2.png')");
+        $asideli.eq(1).css("background-image","url('/resources/images/icon/aside-icon-2-2.png')");
     }, function(){
-        $asideli.eq(1).css("background-image","url(../../resources/images/icon/aside-icon-2-1.png')");
+        $asideli.eq(1).css("background-image","url('/resources/images/icon/aside-icon-2-1.png')");
     });
     $asideli.eq(2).hover(function(){
-        $asideli.eq(2).css("background-image","url('../../resources/images/icon/aside-icon-3-2.png')");
+        $asideli.eq(2).css("background-image","url('/resources/images/icon/aside-icon-3-2.png')");
     }, function(){
-        $asideli.eq(2).css("background-image","url('../../resources/images/icon/aside-icon-3-1.png')");
+        $asideli.eq(2).css("background-image","url('/resources/images/icon/aside-icon-3-1.png')");
     });
     $topbt.hover(function(){
-        $topbt.css("background-image","url('../../resources/images/icon/aside-icon-top.png')");
+        $topbt.css("background-image","url('/resources/images/icon/aside-icon-top.png')");
     }, function(){
-        $topbt.css("background-image","url('../../resources/images/icon/aside-icon-top-2.png')");
+        $topbt.css("background-image","url('/resources/images/icon/aside-icon-top-2.png')");
     });
 
     $asideli.eq(0).click(function(){
         $("html,body").stop().animate({scrollTop:0},800);
-        $asideli.eq(0).css("background-image","url('../../resources/images/icon/aside-icon-1-2.png')");
+        $asideli.eq(0).css("background-image","url('/resources/images/icon/aside-icon-1-2.png')");
     });
     $asideli.eq(1).click(function(){
         $("html,body").stop().animate({scrollTop:$("#con_2").offset().top/1.5},800);
-        $asideli.eq(1).css("background-image","url('../../resources/images/icon/aside-icon-2-2.png')");
+        $asideli.eq(1).css("background-image","url('/resources/images/icon/aside-icon-2-2.png')");
     });
     $asideli.eq(2).click(function(){
         $("html,body").stop().animate({scrollTop:$("#con_3").offset().top/1.2},800);
-        $asideli.eq(2).css("background-image","url('../../resources/images/icon/aside-icon-3-2.png')");
+        $asideli.eq(2).css("background-image","url('/resources/images/icon/aside-icon-3-2.png')");
     });
     $topbt.click(function(){
         $("html,body").stop().animate({scrollTop:0},800);
-        $asideli.eq(0).css("background-image","url('../../resources/images/icon/aside-icon-1-2.png')");
+        $asideli.eq(0).css("background-image","url('/resources/images/icon/aside-icon-1-2.png')");
     });
 });
